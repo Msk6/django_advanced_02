5625f60e0753176e781d2f17298819021dee528b
Msk6 <Maha.s.kh@hotmail.com>
2020-09-13 14:50:18 +0300

    Solution 1

--- stores/views.py
+++ stores/views.py
@@ -10,4 +10,26 @@ def store_list(request):
     return render(request, 'store_list.html', context)
 
 
+def create_view(request):
+    form = StoreModelForm()
+    if request.method == 'POST':
+        form = StoreModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    context = {
+        "form": form
+    }
+    return render(request, 'create_store.html', context)
+
+
+def store_detail(request, store_slug):
+    context = {
+        "store": Store.objects.get(slug=store_slug)
+    }
+    return render(request, 'store_detail.html', context)
+
+
+
+
 
